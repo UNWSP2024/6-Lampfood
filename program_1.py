3c1633030f178c03ad12ea343b7df471e6ac7ff7
Elliott Morris <131560931+Lampfood@users.noreply.github.com>
2026-02-03 22:19:23 -0600

    Week 6 program 1.py

--- program_1.py
+++ program_1.py
@@ -1,15 +1,32 @@
-
-# Program #1: Random Dice
-# Write a "randDice" function (with no input) that randomly chooses two numbers between 1 and 6 (inclusive) and then adds them (this is to simulate the rolling of 2 dice).  
-# The dice sum will be the output of this function.
+#Elliott Morris, 2/4/2026, Dice
+import random
 
 def randDice():
-    # Write your logic to generate 2 numbers between 1 and 6 here
+    #roll two dice (1-6) and return their sum
+    die1 = random.randint(1,6)
+    die2 = random.randint(1,6)
+    return die1 + die2
 
-    # Sum 2 numbers
+def get_suffix(n):
+    #gets the suffix (1st, 2nd, 3rd) for good displaying
+    #Handles 11, 12, and 13
+    if 10 <= n % 100 <= 20: #finds if a number is between 10 and 20
+        return "th"
+    #handles other endings
+    else:
+        return {1:"st", 2:"nd", 3:"rd"}.get(n%10, "th") #assigns the correct suffix
 
-    # return sum to calling function
+def main():
+    total = 0
+    for count in range(1,101):
+        dice_result = randDice()
+        suffix = get_suffix(count)
+        total += dice_result
+        print(f"The {count}{suffix} dice result is: {dice_result}")
 
-#########
-# Then write a mainline that calls the "randDice" function 100 times in a for loop.  
-# The mainline then prints the average of the 100 rolls, rounded to the nearest 0.01.
+    average = total / 100
+    print(f"The total of all rolls was {total}.")
+    print(f"The average of all rolls was {average:.2f}")
+
+if __name__ == "__main__":
+    main()
